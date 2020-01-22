07bbf48d5e56d7fc1a166f66f6b292fa0371d217
tommy <tomnelis53@gmail.com>
2020-01-22 18:51:58 +0100

    async storage basics in place

--- App/components/wholes/BudgetApp.js
+++ App/components/wholes/BudgetApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Modal } from "react-native";
+import { StyleSheet, View, AsyncStorage } from "react-native";
 
 import GraphComponent from "./GraphComponent";
 import TransactionList from "./TransactionList";
@@ -8,15 +8,36 @@ import ModalScreen from './ModalScreen';
 
 const BudgetApp = () => {
     const [modalActive, setModalActive] = useState(false);
+    const [transactionArray, setTransactionArray] = useState([]);
+
+    const storeData = async () => {
+        try {
+            await AsyncStorage.setItem('@MySuperStore:key', 'I like to save it.');
+        } catch (error) {
+            // Error saving data
+        }
+    };
+
+    const retrieveData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('TASKS');
+            if (value !== null) {
+                // We have data!!
+                console.log(value);
+            }
+        } catch (error) {
+            // Error retrieving data
+        }
+    };
 
   return (
     <View style={styles.container}>
         <GraphComponent/>
         <TransactionList />
         {!modalActive && 
-          <ModalButton
-              openModal={() => setModalActive(true)}
-          />
+            <ModalButton
+                openModal={() => setModalActive(true)}
+            />
         }
         {modalActive && 
             <ModalScreen
@@ -28,10 +49,10 @@ const BudgetApp = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: '#2b4369',
-      alignItems: 'center'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#2b4369',
+        alignItems: 'center'
+    }
 });
 export default BudgetApp;
