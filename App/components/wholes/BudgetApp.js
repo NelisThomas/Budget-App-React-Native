7d6ef71294fae7bd4802fd07e93f2f425e6498de
a16jenja <49199287+a16jenja@users.noreply.github.com>
2020-01-22 21:31:48 +0100

    Transaction list renders from global state now

--- App/components/wholes/BudgetApp.js
+++ App/components/wholes/BudgetApp.js
@@ -4,15 +4,43 @@ import { StyleSheet, View, AsyncStorage } from "react-native";
 import GraphComponent from "./GraphComponent";
 import TransactionList from "./TransactionList";
 import ModalButton from "./ModalButton";
-import ModalScreen from './ModalScreen';
+import ModalScreen from "./ModalScreen";
 
 const BudgetApp = () => {
     const [modalActive, setModalActive] = useState(false);
-    const [transactionArray, setTransactionArray] = useState([]);
+    const [transactionArray, setTransactionArray] = useState([
+        {
+            id: 1,
+            date: "22-01-2020",
+            value: +50,
+            type: "Income"
+        },
+        {
+            id: 2,
+            date: "23-01-2020",
+            value: -100,
+            type: "Expense"
+        },
+        {
+            id: 3,
+            date: "17-01-2020",
+            value: +5000,
+            type: "Income"
+        },
+        {
+            id: 4,
+            date: "12-01-2020",
+            value: -250,
+            type: "Expense"
+        }
+    ]);
 
     const storeData = async () => {
         try {
-            await AsyncStorage.setItem('@MySuperStore:key', 'I like to save it.');
+            await AsyncStorage.setItem(
+                "@MySuperStore:key",
+                "I like to save it."
+            );
         } catch (error) {
             // Error saving data
         }
@@ -20,7 +48,7 @@ const BudgetApp = () => {
 
     const retrieveData = async () => {
         try {
-            const value = await AsyncStorage.getItem('TASKS');
+            const value = await AsyncStorage.getItem("TASKS");
             if (value !== null) {
                 // We have data!!
                 console.log(value);
@@ -30,29 +58,25 @@ const BudgetApp = () => {
         }
     };
 
-  return (
-    <View style={styles.container}>
-        <GraphComponent/>
-        <TransactionList />
-        {!modalActive && 
-            <ModalButton
-                openModal={() => setModalActive(true)}
-            />
-        }
-        {modalActive && 
-            <ModalScreen
-              closeModal={() => setModalActive(false)}
-            />
-        }
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <GraphComponent />
+            <TransactionList transactionList={transactionArray} />
+            {!modalActive && (
+                <ModalButton openModal={() => setModalActive(true)} />
+            )}
+            {modalActive && (
+                <ModalScreen closeModal={() => setModalActive(false)} />
+            )}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#2b4369',
-        alignItems: 'center'
+        backgroundColor: "#2b4369",
+        alignItems: "center"
     }
 });
 export default BudgetApp;
--- App/components/wholes/TransactionList.js
+++ App/components/wholes/TransactionList.js
@@ -2,38 +2,11 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, FlatList, SafeAreaView } from "react-native";
 import { List, ListItem } from "react-native-elements";
 
-const TransactionList = () => {
-    const [listOfTransactions, setListOfTransactions] = useState([
-        {
-            id: 1,
-            date: "22-01-2020",
-            value: +50,
-            type: "Income"
-        },
-        {
-            id: 2,
-            date: "23-01-2020",
-            value: -100,
-            type: "Expense"
-        },
-        {
-            id: 3,
-            date: "17-01-2020",
-            value: +5000,
-            type: "Income"
-        },
-        {
-            id: 4,
-            date: "12-01-2020",
-            value: -250,
-            type: "Expense"
-        }
-    ]);
-
+const TransactionList = props => {
     return (
         <View style={styles.container}>
             <FlatList
-                data={listOfTransactions}
+                data={props.transactionList}
                 renderItem={({ item }) => (
                     <View style={styles.itemRow}>
                         <View
